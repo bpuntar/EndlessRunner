5d535da08cdae207bc9450e9cdeddee1090fe346
Brandon Apuntar <bapuntar@ucsc.edu>
2025-02-03 12:30:45 -0800

    added rocket patrol temporary menu

---
+++ src/main.js
@@ -0,0 +1,22 @@
+// Name: Brandon Apuntar
+// Game Title:
+// Approximate Time spent working:
+// Creative Tilt:
+
+'use strict';
+
+let config = {
+    type: Phaser.AUTO,
+    height: 640, 
+    width: 960,
+    scene: [Menu, Play]
+}
+
+let game = new Phaser.Game(config)
+
+//reserve keyboard bindings
+let keyLEFT
+
+//set UI sizes
+let borderUISize = game.config.height / 15
+let borderPadding = borderUISize / 3
--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -1,22 +1,44 @@
-// Name: Brandon Apuntar
-// Game Title:
-// Approximate Time spent working:
-// Creative Tilt:
+class Menu extends Phaser.Scene {
+    constructor() {
+        super("menuScene")
+    }
 
-'use strict';
+    preload() {
+        //load images/tile sprites
+        this.load.image('starfield', './assets/starfield.png')
+    }
+    
+    create() {
 
-let config = {
-    parent: 'myGame',
-    type: Phaser.auto,
-    height: 640, 
-    width: 960,
-    scale: {
-        autoCenter: Phaser.Scale.CENTER_BOTH
-    },
-    physics: {
-        default: 'arcade',
-        arcade: {
+        let menuConfig = {
+            fontFamily: 'Courier',
+            fontSize: '28px',
+            backgroundColor: '#F3B141',
+            color: '#843605',
+            align: 'right',
+            padding: {
+            top: 5,
+            bottom: 5,
+            },
+            fixedWidth: 0
         }
-    },
-    scene: [Title, Play]
+        
+        this.add.text(game.config.width/2, game.config.height/2 - borderUISize - borderPadding, 'ROCKET PATROL', menuConfig).setOrigin(0.5)
+        this.add.text(game.config.width/2, game.config.height/2, 'Use <> arrows to move & (F) to fire', menuConfig).setOrigin(0.5)
+        menuConfig.backgroundColor = '#00FF00'
+        menuConfig.color = '#000'
+        this.add.text(game.config.width/2, game.config.height/2 + borderUISize + borderPadding, 'Press < for Novice or > for Expert', menuConfig).setOrigin(0.5)
+        // define keys
+        keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT)
+
+    }
+
+    update() {
+        if (Phaser.Input.Keyboard.JustDown(keyLEFT)) {
+            this.scene.start('playScene')
+        }
+    }
+
+
+
 }
